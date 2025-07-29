c757534bbaf7ef6999a5a7b4c71812199769095b
Burak Ünal <149411618+bburaakk@users.noreply.github.com>
2025-07-29 09:11:47 +0300

    Proje modüler hale getirildi. - login_screen - rapor_ekrani - ogrenci_ekle_ekrani - ogrencisecimekrani - signup_screen - test_screen Eklendi.

---
+++ algomind/screens/test_screen.py
@@ -0,0 +1,22 @@
+from kivy.uix.screenmanager import Screen
+from kivy.properties import NumericProperty, StringProperty
+from kivy.clock import Clock
+
+
+class TestScreen(Screen):
+    time_elapsed = NumericProperty(0)
+    formatted_time = StringProperty("00:00")
+
+    def on_enter(self, *args):
+        Clock.schedule_interval(self.update_timer, 1)
+
+    def on_leave(self, *args):
+        Clock.unschedule(self.update_timer)
+        self.time_elapsed = 0
+        self.formatted_time = "00:00"
+
+    def update_timer(self, dt):
+        self.time_elapsed += 1
+        minutes = self.time_elapsed // 60
+        seconds = self.time_elapsed % 60
+        self.formatted_time = f"{minutes:02d}:{seconds:02d}"
