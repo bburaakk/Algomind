5757d293f4e4bb3ed979362cde84279e48c75328
Elif FIRINCI <100481883+elifirinci@users.noreply.github.com>
2025-08-02 13:15:23 +0300

    Update models.py

--- fastapi/models.py
+++ fastapi/models.py
@@ -1,11 +1,20 @@
 # models.py
-from sqlalchemy import Column, Integer, String
-from .database import Base
+from sqlalchemy import Column, Integer, String, DateTime, CheckConstraint
+from datetime import datetime
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-    role = Column(String)  # "teacher" ya da "parent"
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String(20), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    __table_args__ = (
+        CheckConstraint(role.in_(["öğretmen", "veli"]), name="check_valid_role"),
+    )
