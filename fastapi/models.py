4e3037ef79ea6c2f17930f4c336b7a9ba4cbc9b1
Elif FIRINCI <100481883+elifirinci@users.noreply.github.com>
2025-08-03 21:37:30 +0300

    Update models.py

--- fastapi/models.py
+++ fastapi/models.py
@@ -1,7 +1,9 @@
-# models.py
-from sqlalchemy import Column, Integer, String, DateTime, CheckConstraint
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime, CheckConstraint, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from datetime import datetime
 from database import Base
+
 
 class User(Base):
     __tablename__ = "users"
@@ -18,3 +20,50 @@ class User(Base):
     __table_args__ = (
         CheckConstraint(role.in_(["öğretmen", "veli"]), name="check_valid_role"),
     )
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    age = Column(Integer, nullable=False)
+    sensitivities = Column(Text)
+    interests = Column(Text)
+    education_status = Column(Text)
+    communication_level = Column(Text)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    tests = relationship("Test", back_populates="student", cascade="all, delete")
+    reports = relationship("Report", back_populates="student", cascade="all, delete")
+
+
+class Test(Base):
+    __tablename__ = "tests"
+
+    id = Column(Integer, primary_key=True, index=True)
+    test_title = Column(String(255), nullable=False)
+    student_id = Column(Integer, ForeignKey("students.id", ondelete="CASCADE"), nullable=False)
+
+    student = relationship("Student", back_populates="tests")
+    reports = relationship("Report", back_populates="test", cascade="all, delete")
+
+   class Report(Base):
+    __tablename__ = "reports"
+
+    id = Column(Integer, primary_key=True, index=True)
+    test_id = Column(Integer, ForeignKey("tests.id", ondelete="CASCADE"), nullable=False)
+    student_id = Column(Integer, ForeignKey("students.id", ondelete="CASCADE"), nullable=False)
+    test_title = Column(String(255), nullable=False)
+    
+    ogrenci_adi = Column(Text)
+    konu = Column(Text)
+    dogru_cevap = Column(Integer)
+    yanlis_cevap = Column(Integer)
+    bos_cevap = Column(Integer)
+    toplam_soru = Column(Integer)
+    yuzde = Column(Float)
+    sure = Column(Float)
+
+    test = relationship("Test", back_populates="reports")
+    student = relationship("Student", back_populates="reports") students = relationship("Student", backref="user", cascade="all, delete")
