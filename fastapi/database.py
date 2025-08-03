784de6f0861187d4bd33ff8b0ab150d5d6d70775
Elif FIRINCI <100481883+elifirinci@users.noreply.github.com>
2025-08-03 21:25:02 +0300

    Api database son hali

--- fastapi/database.py
+++ fastapi/database.py
@@ -1,4 +1,3 @@
-# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -7,10 +6,16 @@ DB_USER = "postgres"
 DB_PASSWORD = "BtkTestApp!123"
 DB_HOST = "34.22.172.29"
 DB_PORT = "5432"
-DB_NAME = "otizm_db"  # senin oluşturduğun veritabanı adı buysa
+DB_NAME = "postgres"
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
